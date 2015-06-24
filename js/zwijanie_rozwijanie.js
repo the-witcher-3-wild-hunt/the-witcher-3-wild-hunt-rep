329d8cf6f4506590ad88c14aeac4c283f666ffde
Mariusz Marciniuk <mariusz2048@gmail.com>
2015-06-24 23:22:09 +0200

    Poprawienie jquery

--- js/zwijanie_rozwijanie.js
+++ js/zwijanie_rozwijanie.js
@@ -10,10 +10,14 @@
                 } else if($(this).text() === '-') {
                     $(this).text('+').parent().parent().find('p').slideUp();
                 } else {
-                    switch ($(this).attr('id')) {
+                    if($(this).attr('id') === 'przelaczanie') {
+                        var link = $(this).attr('href');
+                        window.location.href = link;
+                    }
+                    /*switch ($(this).attr('id')) {
                         case 'galeria': window.location.href = 'http://the-witcher-3-wild-hunt.github.io/the-witcher-3-wild-hunt-rep/galeria.html'; break;
                         case 'stronaGlowna': window.location.href = 'http://the-witcher-3-wild-hunt.github.io/the-witcher-3-wild-hunt-rep/index.html'; break;
-                    }
+                    }*/
                 }
             });
         });
